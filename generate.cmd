a833b14f7a9527eefe78551be3c3a48d27db5d2c
TheBoneJarmer <rmlabruyere@gmail.com>
2019-10-13 20:37:55 +0200

    And removed scripts I wont use anymore

--- generate.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-
-.\tools\srcgen .\src-base .\src-tilargl
--- init.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-
-dotnet publish -c Release -r win-x64 -o tools /p:PublishSingleFile=true ./src-tools/source-generator/SourceGenerator.csproj
