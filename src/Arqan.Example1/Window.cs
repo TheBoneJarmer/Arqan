44eff1a93a9b831c838a1f05a36133dad6307a4a
Ruben Labruyere <info@rubenlabruyere.be>
2020-11-25 15:32:39 +0100

    Updated example 1

--- src/Arqan.Example1/Window.cs
+++ src/Arqan.Example1/Window.cs
@@ -155,11 +155,11 @@ namespace Example1
                 // Update
 
                 // Render a background and enable some stuff for 2d rendering with alpha
-                GL10.glEnable(GL11.GL_BLEND);
-                GL10.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-                GL10.glViewport(0, 0, Width, Height);
-                GL10.glClearColor(0, 0, 0, 1);
-                GL10.glClear(GL11.GL_COLOR_BUFFER_BIT);
+                GL.glEnable(GL.GL_BLEND);
+                GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
+                GL.glViewport(0, 0, Width, Height);
+                GL.glClearColor(0, 0, 0, 1);
+                GL.glClear(GL.GL_COLOR_BUFFER_BIT);
 
                 // Render
 
