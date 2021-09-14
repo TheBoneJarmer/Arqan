28c01d7b924bbf7604186d2b16395f8d8039b46a
Ruben Labruyere <info@rubenlabruyere.be>
2021-09-14 19:37:38 +0200

    Updated publish script

--- publish.ps1
+++ publish.ps1
@@ -1,7 +1,7 @@
 ﻿$branch = git branch --show-current;
 
 Remove-Item *.nupkg
-dotnet pack -c Release -o . ./src/TiledCS.csproj
+dotnet pack -c Release -o . ./src/Arqan/Arqan.csproj
 
 if ($branch -eq "develop")
 {
